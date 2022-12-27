9619d2c78d95f7c031d6f2f04bdc9ec88ef09463
glaciar-pp <glaciar.pp@gmail.com>
2022-12-28 00:00:00 +0900

    2022-12-27 Day2

---
+++ src/main/java/com/mulcam/demo/entity/FileEntity.java
@@ -0,0 +1,38 @@
+package com.mulcam.demo.entity;
+
+public class FileEntity {
+	private String fileName;
+	private String contentType;
+
+	public FileEntity() {
+
+	}
+
+	public FileEntity(String fileName, String contentType) {
+		super();
+		this.fileName = fileName;
+		this.contentType = contentType;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	@Override
+	public String toString() {
+		return "FileEntity [fileName=" + fileName + ", contentType=" + contentType + "]";
+	}
+
+}
